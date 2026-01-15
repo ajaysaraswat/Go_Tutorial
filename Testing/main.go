58453483dde7591bbce5fd39afeb9257bc4f4b72
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2026-01-15 14:16:39 +0530

    adding ... in the print statement

--- Testing/main.go
+++ Testing/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Learning git commands")
+	fmt.Println("Learning git commands....")
 	oddArray := []string{"1","3","5","7"}
 	for index,odd := range oddArray {
 		fmt.Printf("%v odd no is %v \n",index,odd)
