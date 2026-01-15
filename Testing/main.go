ebc545e33e5de0861ba5dfa02fac012cd1a592e3
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2026-01-15 12:18:15 +0530

    loop furthur

--- Testing/main.go
+++ Testing/main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Learning git commands")
+	oddArray := []string{"1","3","5","7"}
+	for index,odd := range oddArray {
+		fmt.Printf("%v odd no is %v \n",index,odd)
+	}
 }
